580793a7aa7f2530ea70f2d4ec0f54792dd8e5a4
KCF4ng <kcf4ng@gmail.com>
2019-08-14 15:52:24 +0800

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("第一個 Console 應用程式");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("1. Console\n");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("2. 視窗應用程式\n");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("3. 網頁應用程式\n");
+            Console.ResetColor();
+            Console.WriteLine("\'單引號\'");
+            Console.WriteLine("\"雙引號\"");
+            Console.WriteLine("\\反斜線\\");
+            Console.WriteLine("\r\n換行");
+            Console.WriteLine("\t縮排，預設縮排為四格字元");
+
+            //單行註解
+            /*
+             * 多行註解
+             */
+            Console.ReadKey();
         }
     }
 }
